25689503d0913b1cc5d17e9a39538ec94acd415b
Md-nadeeem <nadeem.mohd81423@gmail.com>
2024-02-01 18:12:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Addtodo from "./component/Addtodo"
+import Todos from "./component/Todo"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>learn about redux toolkit</h1>
+    <Addtodo/>
+    <Todos/>
+      
+    </>
+  )
+}
+
+export default App
